98000e6b0332787a4e7648aed4b3211ebc25f2ce
DydiRah <124720797+DydiRah@users.noreply.github.com>
2023-02-08 05:52:17 +0100

    Update Echange.php

--- application/controllers/Echange.php
+++ application/controllers/Echange.php
@@ -1,11 +1,21 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 class Echange extends CI_Controller {
-    public function demande($idO1){
-        $this->load->model('Echange_model');
-        $idUser=2;
-        $this->Echange_model->insertDemande($idO1,$idUser);
-        redirect(site_url('categorie/getAllObjet'));
+    public function demande(){
+        $this->load->model('echange_model');
+        $idU1 = $this->session->userdata('idUser');
+        $idU2 = $this->input->get('idU2');
+        $ido1 = $this->input->get('ido1');
+        $ido2 = $this->input->get('ido2');
+        $this->echange_model->insertEchange($idU1,$idU2,$ido1,$ido2);
+        redirect(site_url('accueil'));
+    }
 
+    public function acceptation(){
+        $this->load->model('echange_model');
+        $idUser = $this->session->userdata('idUser');
+        $data = array();
+        $data['demandes'] = $this->echange_model->getDemande($idUser);
+        $this->load->view('acceptation',$data);
     }
 }
