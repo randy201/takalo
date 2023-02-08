dc9ec5d4666812f514463876523397fb3e72cba4
randy201 <108166894+randy201@users.noreply.github.com>
2023-02-08 07:58:09 +0300

    Update allcategorie.php

--- application/views/allcategorie.php
+++ application/views/allcategorie.php
@@ -75,7 +75,7 @@
                     <div class="col-md-6 col-lg-3 d-flex align-items-stretch" data-aos="fade-up">
                         <div class="card">
                             <span class="card-img" >
-                                <img src="<?php echo site_url('assets/img') ?>/original.jpeg" alt="">
+                                <img src="<?php echo site_url('assets/img') ?>/original.jpeg" alt=""  width="250px">
                             </span>
                             <div class="card-body" >
                                 <h5 class="card-title">nom_Objet: <?php echo $object['nomObjet'];?> </h5>
